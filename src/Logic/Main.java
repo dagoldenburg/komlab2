0d3dfb921f79e16a07c2a694798252f6cdcb12f7
dagoldenburg <dagoldenburg@gmail.com>
2017-10-16 18:09:04 +0200

    Fixat allt nice bra

--- src/Logic/Main.java
+++ src/Logic/Main.java
@@ -29,22 +29,26 @@ public class Main {
         t.start();
         stateHandler = new StateHandler();
         stateHandler.initStates();
-        Scanner s = new Scanner(System.in);
         while (true) {
             if (Main.stateHandler.getCurrentState() instanceof WaitingState) {
+                Scanner s = new Scanner(System.in);
                 System.out.println("Welcome, write \"call <ip>:<port>\" to call someone");
                 String input = s.nextLine();
-                if (input.substring(0, 5).equalsIgnoreCase("call ")) {
-                    String temp = input.substring(5);
-                    String[] info = temp.split(":");
-                    try {
-                        StateHandler.ip = info[0];
-                        StateHandler.port = Integer.parseInt(info[1]);
-                        Main.stateHandler.invokeSendInvite();
-                    } catch (ArrayIndexOutOfBoundsException e) {
-                        System.out.println("Need <ip>:<port>, Example: 0.0.0.0:9999");
-                    }
-                } else System.out.println("Invalid input");
+                try {
+                    if (input.substring(0, 5).equalsIgnoreCase("call ")) {
+                        String temp = input.substring(5);
+                        String[] info = temp.split(":");
+                        try {
+                            StateHandler.ip = info[0];
+                            StateHandler.port = Integer.parseInt(info[1]);
+                            Main.stateHandler.invokeSendInvite();
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("Need <ip>:<port>, Example: 0.0.0.0:9999");
+                        }
+                    } else System.out.println("Invalid input");
+                }catch(StringIndexOutOfBoundsException){
+
+                }
             }
         }
     }
