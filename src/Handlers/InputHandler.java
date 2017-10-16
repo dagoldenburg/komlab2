81021acf5700d426651af36715cc32ced93e1b70
dagoldenburg <dagoldenburg@gmail.com>
2017-10-16 17:59:43 +0200

    Fixat allt nice bra

--- src/Handlers/InputHandler.java
+++ src/Handlers/InputHandler.java
@@ -14,20 +14,21 @@ public class InputHandler implements Runnable {
     @Override
     public void run() {
         Scanner s = new Scanner(System.in);
-        while(true) {
+        while (true) {
             String input = s.nextLine();
-            if (input.length() > 4) {
-                if (input.substring(0, 5).equalsIgnoreCase("call ") && Main.stateHandler.getCurrentState() instanceof WaitingState) {
+            if (Main.stateHandler.getCurrentState() instanceof WaitingState) {
+                System.out.println("Welcome, write \"call <ip>:<port>\" to call someone");
+                if (input.substring(0, 5).equalsIgnoreCase("call ")) {
                     String temp = input.substring(5);
                     String[] info = temp.split(":");
                     try {
                         StateHandler.ip = info[0];
                         StateHandler.port = Integer.parseInt(info[1]);
                         Main.stateHandler.invokeSendInvite();
-                    }catch(ArrayIndexOutOfBoundsException e){
+                    } catch (ArrayIndexOutOfBoundsException e) {
                         System.out.println("Need <ip>:<port>, Example: 0.0.0.0:9999");
                     }
-                }else System.out.println("Invalid input");
+                } else System.out.println("Invalid input");
             }
         }
     }
